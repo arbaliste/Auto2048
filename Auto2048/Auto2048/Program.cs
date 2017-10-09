902f1fee31f04c65cb900682bc0529a436c79aae
52 <jaren.mendelsohn@gmail.com>
2017-10-08 18:31:56 -0700

    Parallelized and removed sleep

--- Auto2048/Auto2048/Program.cs
+++ Auto2048/Auto2048/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using OpenQA.Selenium.Remote;
 using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace AutomaticSnake
 {
@@ -13,72 +14,62 @@ namespace AutomaticSnake
     {
         static void Main(string[] args)
         {
-            RemoteWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://arbaliste.github.io/Auto2048");
 
             int size = 4 * 4;
-            int maxTrials = 5;
+            int maxTrials = 20;
             Trial[] trials = Enumerable.Range(0, maxTrials).Select(x => new Trial()
             {
                 Network = new NeuralNetwork(new int[] { size, 9, 1 }, Util.Sigmoid),
-                Fitness = 0
+                Fitness = 0,
+                Driver = new ChromeDriver()
             }).ToArray();
             Trial bestTrial = trials[0];
+
+            foreach (Trial t in trials)
+                t.Driver.Navigate().GoToUrl("http://arbaliste.github.io/Auto2048");
 
             int gen = 1;
 
             while (true)
             {
                 Console.WriteLine("Running generation " + gen);
-                for (int trialNum = 0; trialNum < maxTrials; trialNum++)
+                Parallel.ForEach(trials, (trial) =>
                 {
+                    trial.Driver.FindElementByClassName("restart-button").Click();
+                    System.Threading.Thread.Sleep(200);
                     double[] previous = new double[size];
                     while (true)
                     {
-                        // Exits the program when the browser is closed
-                        // e has to be here because it will be optimized out?
-                        var e = "";
-                        try
-                        {
-                            e = driver.Title;
-                        }
-                        catch
-                        {
-                            driver.Dispose();
-                            return;
-                        }
-
-                        System.Threading.Thread.Sleep(200);
-                        var manager = (Dictionary<string, dynamic>)driver.ExecuteScript("return gameManager");
+                        var manager = (Dictionary<string, dynamic>)trial.Driver.ExecuteScript("return gameManager");
                         bool over = manager["over"];
                         long score = manager["score"];
                         double[] board = ((IEnumerable<object>)(manager["grid"]["cells"])).Cast<IEnumerable<dynamic>>().SelectMany(x => x).Select(x => Math.Log((x?["value"]*2) ?? 2, 2) / 12d).Cast<double>().ToArray();
 
                         if (over || board.SequenceEqual(previous))
                         {
-                            trials[trialNum].Fitness = score;
+                            trial.Fitness = score;
                             break;
                         }
 
-                        double data = trials[trialNum].Network.Run(board)[0];
+                        double data = trial.Network.Run(board)[0];
                         string sendKeys = "";
                         if (data < 0.25) sendKeys = "w";
                         else if (data < 0.5) sendKeys = "a";
                         else if (data < 0.75) sendKeys = "s";
                         else sendKeys = "d";
 
-                        driver.Keyboard.SendKeys(sendKeys);
+                        trial.Driver.Keyboard.SendKeys(sendKeys);
                         //System.Threading.Thread.Sleep(100);
                         board.CopyTo(previous, 0);
                     }
-                    Console.WriteLine(" - Trial " + trialNum + ": " + trials[trialNum].Fitness);
-                    driver.FindElementByClassName("restart-button").Click();
-                }
+                });
+                for (int i = 0; i < trials.Length; i++)
+                    Console.WriteLine(" - Trial " + i + ": " + trials[i].Fitness);
                 bestTrial = trials.OrderBy(x => x.Fitness).Last();
                 for (int i = 0; i < trials.Length; i++)
                 {
                     trials[i].Network = NeuralNetwork.Cross(bestTrial.Network, trials[i].Network);
-                    if (trials[i].Network != bestTrial.Network) trials[i].Network.Mutate(0.25);
+                    if (trials[i] != bestTrial) trials[i].Network.Mutate(0.25);
                 }
                 Console.WriteLine(" - Best: " + bestTrial.Fitness);
                 gen++;
--- Auto2048/Auto2048/Trial.cs
+++ Auto2048/Auto2048/Trial.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium.Remote;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,6 @@ namespace AutomaticSnake
     {
         public NeuralNetwork Network;
         public double Fitness;
+        public RemoteWebDriver Driver;
     }
 }
