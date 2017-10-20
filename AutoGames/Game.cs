199ae581a1de95be8100f621c825613057084263
jaren <jaren.mendelsohn@gmail.com>
2017-10-19 20:25:29 -0700

    Common game code

--- AutoGames/Game.cs
+++ AutoGames/Game.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using NeuralNetwork;
+using Newtonsoft.Json;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +13,62 @@ namespace AutoGames
 {
     public abstract class Game
     {
-        public abstract void Run();
+        const int MaxDrivers = 10;
+        const int TrialsPerDriver = 10;
+        const int MaxTrials = MaxDrivers * TrialsPerDriver;
+        const string backupFile = "network.json";
+
+        public string Url;
+
+        public Game(string url)
+        {
+            Url = url;
+        }
+
+        protected abstract Network GenerateNetwork();
+        protected abstract void RunTrial(RemoteWebDriver driver, Trial trial);
+
+        public void Run()
+        {
+            int generation = 1;
+
+            ChromeDriver[] drivers = Enumerable.Range(0, MaxDrivers).Select(x => new ChromeDriver()).ToArray();
+            Trial[] trials = Enumerable.Range(0, MaxTrials).Select(x => new Trial()
+            {
+                Network = GenerateNetwork(),
+                Fitness = 0
+            }).ToArray();
+
+            if (File.Exists(backupFile))
+            {
+                Console.WriteLine("Generating networks from backup");
+                var layers = JsonConvert.DeserializeObject<List<List<Layer>>>(File.ReadAllText(backupFile));
+                for (int i = 0; i < trials.Length; i++)
+                {
+                    trials[i].Network.Layers = layers[i];
+                }
+            }
+            else
+            {
+                Console.WriteLine("Generating networks randomly");
+            }
+
+            foreach (var driver in drivers)
+                driver.Navigate().GoToUrl("http://arbaliste.github.io/AutoGames/" + Url);
+
+
+            while (true)
+            {
+                Parallel.ForEach(drivers, (driver, _, driverNum) =>
+                {
+                    for (int trialNum = 0; trialNum < TrialsPerDriver; trialNum++)
+                        RunTrial(driver, trials[driverNum * TrialsPerDriver + trialNum]);
+                });
+                Trial.BreedMethods.Aggressive(trials, 0.3);
+                Console.WriteLine($"Gen {generation} | Max: {trials.Max(x => x.Fitness)} | Avg: {trials.Average(x => x.Fitness)} | Top: {String.Join(", ", trials.OrderByDescending(x => x.Fitness).Take(5).Select(x => x.Fitness))}");
+                File.WriteAllText(backupFile, JsonConvert.SerializeObject(trials.Select(x => x.Network.Layers)));
+                generation++;
+            }
+        }
     }
 }
--- AutoGames/Game2048.cs
+++ AutoGames/Game2048.cs
@@ -7,100 +7,58 @@ using System.Threading.Tasks;
 using NeuralNetwork;
 using Newtonsoft.Json;
 using System.IO;
+using OpenQA.Selenium.Remote;
 
 namespace AutoGames
 {
     public class Game2048 : Game
     {
         const int BoardSize = 4 * 4;
-        const int MaxDrivers = 10;
-        const int TrialsPerDriver = 10;
-        const int MaxTrials = MaxDrivers * TrialsPerDriver;
         const int MaxRepeat = 50;
-        const string backupFile = "network.json";
 
-        public override void Run()
+        public Game2048() : base("2048") { }
+
+        protected override Network GenerateNetwork() => new Network(new int[] { BoardSize + 2, 11, 6, 1 }, Network.ActivationFunctions.TanH, Network.MutateFunctions.GenerateReplacement(1));
+
+        protected override void RunTrial(RemoteWebDriver driver, Trial trial)
         {
-            int generation = 1;
-
-            ChromeDriver[] drivers = Enumerable.Range(0, MaxDrivers).Select(x => new ChromeDriver()).ToArray();
-            Trial[] trials = Enumerable.Range(0, MaxTrials).Select(x => new Trial()
-            {
-                Network = new Network(new int[] { BoardSize + 2, 11, 6, 1 }, Network.ActivationFunctions.TanH, Network.MutateFunctions.GenerateReplacement(1)),
-                Fitness = 0
-            }).ToArray();
-
-            if (File.Exists(backupFile))
-            {
-                Console.WriteLine("Generating networks from backup");
-                var layers = JsonConvert.DeserializeObject<List<List<Layer>>>(File.ReadAllText(backupFile));
-                for (int i = 0; i < trials.Length; i++)
-                {
-                    trials[i].Network.Layers = layers[i];
-                }
-            }
-            else
-            {
-                Console.WriteLine("Generating networks randomly");
-            }
-
-            foreach (var driver in drivers)
-                driver.Navigate().GoToUrl("http://arbaliste.github.io/AutoGames/2048");
-
-
+            driver.FindElementByClassName("restart-button").Click();
+            double[] previous = new double[BoardSize];
+            double previousMove = 0;
+            int repeatNum = 0;
             while (true)
             {
-                Parallel.ForEach(drivers, (driver, _, driverNum) =>
+                var manager = (Dictionary<string, dynamic>)driver.ExecuteScript("return gameManager");
+                bool over = manager["over"];
+                long score = manager["score"];
+                double[] board = ((IEnumerable<object>)(manager["grid"]["cells"])).Cast<IEnumerable<dynamic>>().SelectMany(x => x).Select(x => Math.Log((x?["value"] * 2) ?? 2, 2) / 12d).Cast<double>().ToArray();
+                List<double> inputs = new List<double>();
+                inputs.AddRange(board);
+                inputs.Add(board.SequenceEqual(previous) ? 1 : -1);
+                inputs.Add(previousMove);
+
+                if (board.SequenceEqual(previous))
+                    repeatNum++;
+                else
+                    repeatNum = 0;
+
+                if (over || repeatNum > MaxRepeat)
                 {
-                    for (int trialNum = 0; trialNum < TrialsPerDriver; trialNum++)
-                    {
-                        Trial trial = trials[driverNum * TrialsPerDriver + trialNum];
-                        driver.FindElementByClassName("restart-button").Click();
-                        //System.Threading.Thread.Sleep(200);
-                        double[] previous = new double[BoardSize];
-                        double previousMove = 0;
-                        int repeatNum = 0;
-                        while (true)
-                        {
-                            var manager = (Dictionary<string, dynamic>)driver.ExecuteScript("return gameManager");
-                            bool over = manager["over"];
-                            long score = manager["score"];
-                            double[] board = ((IEnumerable<object>)(manager["grid"]["cells"])).Cast<IEnumerable<dynamic>>().SelectMany(x => x).Select(x => Math.Log((x?["value"] * 2) ?? 2, 2) / 12d).Cast<double>().ToArray();
-                            List<double> inputs = new List<double>();
-                            inputs.AddRange(board);
-                            inputs.Add(board.SequenceEqual(previous) ? 1 : -1);
-                            inputs.Add(previousMove);
+                    trial.Fitness = score;
+                    break;
+                }
 
-                            if (board.SequenceEqual(previous))
-                                repeatNum++;
-                            else
-                                repeatNum = 0;
+                double data = trial.Network.Run(inputs.ToArray())[0];
+                string sendKeys = "";
+                if (data < -0.5) sendKeys = "w";
+                else if (data < 0) sendKeys = "a";
+                else if (data < 0.5) sendKeys = "s";
+                else sendKeys = "d";
 
-                            if (over || repeatNum > MaxRepeat)
-                            {
-                                trial.Fitness = score;
-                                break;
-                            }
+                previousMove = data;
 
-                            double data = trial.Network.Run(inputs.ToArray())[0];
-                            string sendKeys = "";
-                            if (data < -0.5) sendKeys = "w";
-                            else if (data < 0) sendKeys = "a";
-                            else if (data < 0.5) sendKeys = "s";
-                            else sendKeys = "d";
-
-                            previousMove = data;
-
-                            driver.Keyboard.SendKeys(sendKeys);
-                            board.CopyTo(previous, 0);
-
-                        }
-                    }
-                });
-                Trial.BreedMethods.Aggressive(trials, 0.3);
-                Console.WriteLine($"Gen {generation} | Max: {trials.Max(x => x.Fitness)} | Avg: {trials.Average(x => x.Fitness)} | Top: {String.Join(", ", trials.OrderByDescending(x => x.Fitness).Take(5).Select(x => x.Fitness))}");
-                File.WriteAllText(backupFile, JsonConvert.SerializeObject(trials.Select(x => x.Network.Layers)));
-                generation++;
+                driver.Keyboard.SendKeys(sendKeys);
+                board.CopyTo(previous, 0);
             }
         }
     }
