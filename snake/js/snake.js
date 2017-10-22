94de54ce8b2cace4fc24fde3e2ab8a2cdf2adce0
Jaren <jaren.mendelsohn@gmail.com>
2017-10-22 16:12:29 -0700

    Update snake.js

--- snake/js/snake.js
+++ snake/js/snake.js
@@ -121,7 +121,7 @@ SNAKE.Snake = SNAKE.Snake || (function() {
             rowShift = [-1, 0, 1, 0],
             xPosShift = [],
             yPosShift = [],
-            snakeSpeed = 0,
+            snakeSpeed = 15,
             isDead = false,
             isPaused = false;
         function getMode (mode, speed) {
